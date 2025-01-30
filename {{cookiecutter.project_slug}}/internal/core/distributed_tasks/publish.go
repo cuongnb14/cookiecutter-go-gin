0307015a771644a31d5d9d17fdfcd98fca94e93e
Cuong Nguyen <cuongnb14@gmail.com>
2025-01-30 13:49:05 +0700

    refactor publish task

--- {{cookiecutter.project_slug}}/internal/core/distributed_tasks/publish.go
+++ {{cookiecutter.project_slug}}/internal/core/distributed_tasks/publish.go
@@ -7,10 +7,18 @@ import (
 	"{{ cookiecutter.project_slug }}/internal/core/distributed_tasks/tasks"
 )
 
-func PublishDebugTaskTask() {
-	PublishTask(tasks.TypeDebugTask, tasks.DebugTaskPayload{UserID: 1, Message: "hello"}, asynq.Retention(24*time.Hour), asynq.Timeout(3*time.Second))
+func PublishDebugTaskTask() error {
+	return PublishTask(
+		tasks.TypeDebugTask,
+		tasks.DebugTaskPayload{UserID: 1, Message: "hello"},
+		asynq.Retention(24*time.Hour), asynq.Timeout(3*time.Second),
+	)
 }
 
-func PublishSendEmailTask() {
-	PublishTask(tasks.TypeEmailDelivery, tasks.EmailDeliveryPayload{UserID: 1, TemplateID: "welcome-email"}, asynq.Retention(24*time.Hour), asynq.Timeout(3*time.Second))
+func PublishSendEmailTask() error {
+	return PublishTask(
+		tasks.TypeEmailDelivery,
+		tasks.EmailDeliveryPayload{UserID: 1, TemplateID: "welcome-email"}, asynq.Retention(24*time.Hour),
+		asynq.Timeout(3*time.Second),
+	)
 }
